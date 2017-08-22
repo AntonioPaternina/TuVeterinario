1a8dbfa9d740aa2759bf95578f05e0a1b2890451
Antonio Paternina <acpat88@gmail.com>
2017-08-22 11:17:48 -0500

    se termina vista home avance en vista login

--- app/src/main/java/co/edu/poli/tuveterinario/Inicio.java
+++ app/src/main/java/co/edu/poli/tuveterinario/Inicio.java
@@ -2,6 +2,8 @@ package co.edu.poli.tuveterinario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        Button btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

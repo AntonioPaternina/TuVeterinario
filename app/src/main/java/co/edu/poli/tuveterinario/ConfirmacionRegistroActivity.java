76f833c287cb0320171b000212ad8d417e4b98d2
Antonio Paternina <acpat88@gmail.com>
2017-08-23 13:42:40 -0500

    se terminan layouts del grupo lógico de Acceso

---
+++ app/src/main/java/co/edu/poli/tuveterinario/ConfirmacionRegistroActivity.java
@@ -0,0 +1,13 @@
+package co.edu.poli.tuveterinario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmacionRegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion_registro);
+    }
+}
